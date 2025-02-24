77dd09eaf9a95818ae17dae08e595eba2119684e
S1H9L18A9 <sahil8rai@gmail.com>
2025-02-24 20:19:42 +0530

    Update helper_wos.py

--- helper_wos.py
+++ helper_wos.py
@@ -4,7 +4,7 @@ from Android_control.minitouch import AndroidTouchControl
 
 import logging
 import logging.config
-
+import time
 import pandas as pd
 import requests
 import zipfile
@@ -39,8 +39,38 @@ def main():
     logging.info('Starting WoS')
 # adb shell monkey -p com.gof.global -c android.intent.category.LAUNCHER 1
     android._run_adb('shell','monkey','-p','com.gof.global','-c','android.intent.category.LAUNCHER','1')
-    # android.tap(700,100)
-    android.click_on_image(os.path.join((os.path.abspath('')),'template_images','Help.png'))
+    # android.tap(670,70)
+    logging.info('Waiting for game to load up')
+    if (n:=android.wait_for_image(os.path.join((os.path.abspath('')),'template_images','Confirm Button.png'),
+                           timeout=15)):
+        android.tap(*n)
+    else:
+        logging.info('I am being sold some random junk, need to get out')
+        for i in range(3):
+            android.tap(670+i*10,70)
+            if (m:=android.wait_for_image(os.path.join((os.path.abspath('')),'template_images','Confirm Button.png'),
+                           timeout=3)):
+                      
+                android.tap(*m)
+                break
+            elif (m:=android.wait_for_image(os.path.join((os.path.abspath('')),'template_images','World.png'),
+                           timeout=3)):
+                break
+        else:
+            if (m:=android.wait_for_image(os.path.join((os.path.abspath('')),'template_images','World.png'),
+                           timeout=3)):
+                logging.info('Finally in game....I think')
+            else:
+                logging.info('Game blocked me, gege')
+                time.sleep(5)
+                exit()
+    logging.info('Ideally now I am in game')
+    logging.info('Helping perpetually')
+    while True:
+        if  android.click_on_image(os.path.join((os.path.abspath('')),'template_images','Help.png')):
+            if (m:=android.wait_for_image(os.path.join((os.path.abspath('')),'template_images','Back btn.png'),
+                           timeout=3)):
+                android.tap(*m)
     logging.info('Bruh we done')
 
 if __name__ == '__main__':
